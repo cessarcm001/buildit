6732a0ef17b0a18a9aaba02c093e6b80e299c022
Cesar Contreras <cessarcm001@gmail.com>
2024-09-19 22:46:07 -0700

    ahi va

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,28 @@ import {
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import ErrorPage from "./error-page";
+import List from "./pages/list/List";
+import Maquina from "./pages/maquina/Maquina";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
     errorElement: <ErrorPage />,
+    
   },
+  {
+    path: "/Maquinaria",
+    element: <List />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/Maquinaria/:id",
+    element: <Maquina />,
+    errorElement: <ErrorPage />,
+  }
 ]);
+
 
 function App() {
   return (
---
+++ src/pages/maquina/Maquina.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Maquina = () => {
+  return (
+    <div>Maquina</div>
+  )
+}
+
+export default Maquina
