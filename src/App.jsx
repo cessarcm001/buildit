8955c66f86c04b6a5d583f546bd7fb343080bbcb
Cesar Contreras <cessarcm001@gmail.com>
2024-09-19 21:46:18 -0700

    working router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
